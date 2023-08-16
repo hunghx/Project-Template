25b77df987234f6fa00002a0809488dace2ccc58
hunghx <baby10051999@gmail.com>
2023-08-16 20:32:03 +0700

    up

---
+++ src/main/java/ra/projectmd4template/controller/HomeController.java
@@ -0,0 +1,18 @@
+package ra.projectmd4template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping({"/index.html","/","/hahahahaha"})
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/product.html")
+    public String product(){
+        return "product";
+    }
+}
